96ef4bc0e94bf067893d0d665b918c1a8515b61a
Marcos <marcosvazq033@gmail.com>
2023-12-18 09:23:58 +0100

    ADD Dao
    Modificar Dao deportista para eliminar

--- src/dao/DeportistaDao.java
+++ src/dao/DeportistaDao.java
@@ -38,6 +38,54 @@ public class DeportistaDao {
     	return id;
     }
     
+    public String listarParticipaciones(int id) {
+    	String participaciones="";
+    	
+    	try {
+    		conexion = new ConexionBD();
+        	String consulta = "select * from olimpiadas.Participacion";
+        	PreparedStatement pstmt = conexion.getConexion().prepareStatement(consulta);      
+        	ResultSet rs = pstmt.executeQuery();   
+			 while (rs.next()) {
+				 int id_deportista = rs.getInt("id_deportista");
+				 if (id == id_deportista) {
+					 int id_evento = rs.getInt("id_evento");
+					 int id_equipo = rs.getInt("id_equipo");
+					 String edad = rs.getString("edad");
+					 String medalla = rs.getString("medalla");
+					 
+					 rs.close();
+					 consulta = "select * from olimpiadas.Evento where id_evento="+id_evento;
+			         pstmt = conexion.getConexion().prepareStatement(consulta);
+			         rs = pstmt.executeQuery();
+					 rs.next();
+					 String evento = rs.getString("nombre");
+					 
+					 rs.close();
+					 consulta = "select * from olimpiadas.Equipo where id_equipo="+id_equipo;
+			         pstmt = conexion.getConexion().prepareStatement(consulta);
+			         rs = pstmt.executeQuery();
+					 rs.next();
+					 String equipo = rs.getString("nombre");
+					 
+					 participaciones=participaciones
+							+"----------------------------\n"
+							+"  Evento: "+evento+"\n"
+					 		+ "  Equipo: "+equipo+"\n"
+					 		+ "  Edad: "+edad+"\n"
+					 		+ "  Medalla: "+medalla+"\n"
+					 		+"----------------------------\n";
+				}				 
+			 }             
+			rs.close();       
+	        conexion.CloseConexion();
+	    } catch (SQLException e) {	    	
+	    	System.out.println(e.getMessage());
+	    }
+    	
+    	return participaciones;
+    }
+    
     public void insertProducto(int codigo, String nombre, String sexo, int altura, int peso, InputStream imagen) {
     	//Inserta objeto en la BBDD
     	try {
@@ -79,6 +127,30 @@ public class DeportistaDao {
 	    	e.printStackTrace();
 	    }    
         return aeropuertos;    
+    }
+    
+    public Deportista cargarDeportistaConNombre(String nombre)  {
+    	Deportista d = null;
+        try {
+        	conexion = new ConexionBD();        	
+        	String consulta = "select * from Deportista where nombre ='"+nombre+"'";
+        	PreparedStatement pstmt = conexion.getConexion().prepareStatement(consulta);      
+        	ResultSet rs = pstmt.executeQuery();   
+			 while (rs.next()) {
+				 int id = rs.getInt("id_deportista");
+				 String sexo = rs.getString("sexo");
+				 int peso = rs.getInt("peso");
+				 int altura = rs.getInt("altura");
+				 InputStream image = rs.getBinaryStream("foto");
+				 
+				 d = new Deportista(id, nombre, sexo, peso, altura, image);
+			 }
+			 rs.close();       
+	         conexion.CloseConexion();
+	    } catch (SQLException e) {	    	
+	    	e.printStackTrace();
+	    }    
+        return d;    
     }
 
 	public void modProducto(int codigo, String nombre, String sexo, int peso, int altura, InputStream imagen) {
